312cda76358037201ea1a1b9bad582eb2643609c
Rafael Porto <57488043+rafaporto1405@users.noreply.github.com>
2020-10-12 23:00:29 -0300

    no message

---
+++ app_fila_restaurante/android/app/src/main/kotlin/com/porto/app_fila_restaurante/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.porto.app_fila_restaurante
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ buscador_gifs/android/app/src/main/kotlin/com/porto/buscador_gifs/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.porto.buscador_gifs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
