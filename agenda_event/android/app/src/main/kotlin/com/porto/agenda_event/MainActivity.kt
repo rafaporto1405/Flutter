2dd7a1b2560d62c2d69dc2beabc0db41a3f80424
Rafael Porto <57488043+rafaporto1405@users.noreply.github.com>
2020-10-15 08:03:46 -0300

    no message

---
+++ agenda_event/android/app/src/main/kotlin/com/porto/agenda_event/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.porto.agenda_event
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ agenda_event/lib/bloc/calendar.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+import 'base.dart';
+
+class CalendarBloc extends BaseBloc<DateTime> {
+  StreamController<DateTime> _monthStreamController =
+  StreamController<DateTime>();
+  Stream<DateTime> monthStream;
+
+  DateTime _dateTime;
+  DateTime get dateTime => _dateTime ?? DateTime.now();
+
+  DateTime _monthDateTime;
+  DateTime get monthDateTime => _monthDateTime ?? DateTime.now();
+  DateTime collapseDateTime;
+
+  CalendarBloc() {
+    monthStream = _monthStreamController.stream.asBroadcastStream();
+
+    stream.listen((DateTime data) {
+      _dateTime = data;
+    });
+    monthStream.listen((DateTime data) {
+      _monthDateTime = data;
+    });
+  }
+
+  void dispose() {
+    super.dispose();
+    _monthStreamController.close();
+  }
+
+  void updateSelectedDate(DateTime dateTime) {
+    sink.add(dateTime);
+  }
+
+  void updateMonth(DateTime dateTime) {
+    _monthStreamController.sink.add(dateTime);
+  }
+}
